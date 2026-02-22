99f3c07b9757660d2dfe170102ebe05f419d84b6
Clineair <cvh@centralvalleyheli.com>
2026-02-22 13:54:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # ────────────────────────────────────────────────
 st.set_page_config(
     page_title="AgPilot – Aerial Application Performance Tool",
-    page_icon=""⌯✈︎,
+    page_icon="⌯✈︎",
     layout="wide",
     initial_sidebar_state="auto"
 )
