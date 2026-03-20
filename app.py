89566ff0c1dcb0f3a174eee3858d1df0ecd3dbfd
Clineair <cvh@centralvalleyheli.com>
2026-03-19 19:06:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -561,7 +561,8 @@ def show_risk_assessment():
                 - Are you familiar with and used to flying with all your medications?
                 - Are you familiar with your aircraft and aircraft systems?
                 """)
-                st.caption("Log your answers in a notebook or send to cvh@centralvalleyheli.com for review.")
+                st.caption("If you answered No to an of the questions, STOP.
+                Reconsider making the flight or consider mitigation options")
 
     with col_a:
         if st.button("Annual Questions", type="secondary", use_container_width=True):
@@ -581,7 +582,8 @@ def show_risk_assessment():
                 - Have you attended PAASS in the last year?
                 - Have you attended an Operation S.A.F.F. Fly In clinic in the past two years?
                 """)
-                st.caption("Log your answers in a notebook or send to cvh@centralvalleyheli.com for review.")
+                st.caption("If you answered No to an of the questions, STOP.
+                Reconsider making the flight or consider mitigation options")
 
     if total_risk > 30:
         st.info("**Mitigation Recommendations**")
